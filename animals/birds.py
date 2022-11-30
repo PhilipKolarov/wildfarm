ed557de70b7af1b772c8279c5667a51c5b057c57
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2022-11-30 21:16:29 +0200

    Add files via upload

---
+++ animals/birds.py
@@ -0,0 +1,34 @@
+from project_exam.animals.animal import Bird
+
+
+class Owl(Bird):
+    def __init__(self, name, weight, wing_size):
+        super().__init__(name, weight, wing_size)
+
+    @property
+    def foods_allowed(self):
+        return ['Meat']
+
+    @property
+    def weight_incremental(self):
+        return 0.25
+
+    def make_sound(self):
+        return "Hoot Hoot"
+
+
+class Hen(Bird):
+    def __init__(self, name, weight, wing_size):
+        super().__init__(name, weight, wing_size)
+
+    @property
+    def foods_allowed(self):
+        return ['Fruit', 'Vegetable', 'Meat', 'Seed']
+
+    @property
+    def weight_incremental(self):
+        return 0.35
+
+    def make_sound(self):
+        return "Cluck"
+
