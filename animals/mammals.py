982d942b857bab079b4f54c9fc8245d2055fb94b
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2022-11-30 21:44:26 +0200

    Add files via upload

---
+++ animals/mammals.py
@@ -0,0 +1,65 @@
+from project_exam.animals.animal import Mammal
+
+
+class Mouse(Mammal):
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    @property
+    def foods_allowed(self):
+        return ['Fruit', 'Vegetable']
+
+    @property
+    def weight_incremental(self):
+        return 0.1
+
+    def make_sound(self):
+        return "Squeak"
+
+
+class Dog(Mammal):
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    @property
+    def foods_allowed(self):
+        return ['Meat']
+
+    @property
+    def weight_incremental(self):
+        return 0.4
+
+    def make_sound(self):
+        return "Woof!"
+
+
+class Cat(Mammal):
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    @property
+    def foods_allowed(self):
+        return ['Vegetable', 'Meat']
+
+    @property
+    def weight_incremental(self):
+        return 0.3
+
+    def make_sound(self):
+        return "Meow"
+
+
+class Tiger(Mammal):
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    @property
+    def foods_allowed(self):
+        return ['Meat']
+
+    @property
+    def weight_incremental(self):
+        return 1
+
+    def make_sound(self):
+        return "ROAR!!!"
